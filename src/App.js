070cfe6b4b5a1aa4af70bbe8bed4a7d36b4676f6
R Indra Akbar S <rindraakbars@gmail.com>
2021-11-14 18:49:02 +0700

    minor change my-cra-tailwindcss

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { hot } from 'react-hot-loader';
 
 const App = () => (
   <div className='App'>
-    <h1 className='text-4xl'>Hello, World!!!!!</h1>
+    <h1 className='text-5xl'>Hello, World!!!!!</h1>
   </div>
 );
 
