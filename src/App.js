e113c1882f8dd9148d0d80c470303e6410d1e913
R Indra Akbar S <rindraakbars@gmail.com>
2021-11-23 12:40:49 +0700

    update dependency

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { hot } from 'react-hot-loader';
 
 const App = () => (
   <div className='App'>
-    <h1 className='text-5xl'>Hello, World!!!!!</h1>
+    <h1 className='text-5xl font-bold border h-[100px]'>Hello, World!!!!!</h1>
   </div>
 );
 
